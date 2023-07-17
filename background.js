1350b9b3d73e9cab851878914422b5f87d2b9d86
jianqiang <jianqiang@echronos.com>
2023-07-17 15:29:03 +0800

    first commit

---
+++ background.js
@@ -0,0 +1,20 @@
+console.log("铠甲合体~");
+// 创建右键菜单选项
+chrome.contextMenus.create({
+    id: "xiaoer_cloud_baike",
+    title: "热词翻译&百科",
+    contexts: ["selection"]
+  });
+  
+  // 监听右键菜单点击事件
+  chrome.contextMenus.onClicked.addListener(function(info) {
+    if (info.menuItemId === "xiaoer_cloud_baike") {
+      // 获取选中的文本
+      var selectedText = info.selectionText;
+      // console.log(selectedText)
+      // 发送消息给 content script
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "showInterpretations", word: selectedText });
+      });
+    }
+  });
